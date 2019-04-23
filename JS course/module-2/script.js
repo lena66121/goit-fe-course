b7b74677d9905b4159ad4311b38a150919077c5c
Lena <2863040@ukr.net>
2019-04-23 19:28:57 +0300

    hw2 fix

--- JS course/module-2/script.js
+++ JS course/module-2/script.js
@@ -6,50 +6,53 @@ let total = 0;
 
 while (true) {
   input = prompt('Введите число');
+
   if (input === null) {
     break;
-  } else if (isNaN(input) || input === '')  {
+  } 
+  
+  if (Number.isNaN(Number(input)))  {
     alert('Было введено не число, попробуйте еще раз');
-  } else {
-  numbers.push(Number(input));
+  }  else if (Number.isNaN(Number(input)) === false) {
+    numbers.push(Number(input));
   }
+  
+  
 }
 
-for (let i = 0, max = numbers.length; i < max; i += 1) {
-    if (numbers.length === 2) {
-    total = numbers[0] + numbers[1];
-    console.log(`Общая сумма чисел равна ${total}`);
-    break;
-    } else if (numbers.length > 2){
-    sum = numbers[0] + numbers[1];
-    total = sum + numbers[i+1];
-    console.log(`Общая сумма чисел равна ${total}`);
-    break;
-  }
+for (const number of numbers) {
+  total += number
 }
+
+if (numbers.length !== 0) {
+console.log(`Общая сумма чисел равна ${total}`);
+} else {
+  alert('Вы не ввели ни одного числа');
+}
+
 
 // Task 2 
 
-const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
-let attemptsLeft = 3;
-let message;
-let i = 0;
+// const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+// let attemptsLeft = 3;
+// let message;
 
-  do {
-  attemptsLeft -= 1;
-  const userPassword = prompt('Введите пароль');
+//   do {
+//   attemptsLeft -= 1;
+//   const userPassword = prompt('Введите пароль');
 
-  if (userPassword === null) break;
+//   if (userPassword === null) break;
+  
+//   if (!attemptsLeft) {
+//     alert('У вас закончились попытки, аккаунт заблокирован!');
+//     break;
+//   }
 
-  passwords.includes(userPassword) ? message = 'Добро пожаловать!' :
-  message = `Неверный пароль, у вас осталось ${attemptsLeft} попыток`;
-  alert(message);
-
-   if (attemptsLeft === 0) {
-    alert('У вас закончились попытки, аккаунт заблокирован!');
-  } else if (passwords.includes(userPassword) === true) break;
-
-} while (attemptsLeft >= 1 && attemptsLeft <= 3)
+//   message = (passwords.includes(userPassword)) ? 'Добро пожаловать!' :
+//   `Неверный пароль, у вас осталось ${attemptsLeft} попыток`;
+//   alert(message);
+    
+// } while (attemptsLeft || passwords.includes(userPassword) === false)
 
 
 
