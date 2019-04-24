78c63f353bfaeea3b861087efbee9e0a96da2bee
Lena <2863040@ukr.net>
2019-04-24 18:32:58 +0300

    hw2 fix2

--- JS course/module-2/script.js
+++ JS course/module-2/script.js
@@ -11,48 +11,50 @@ while (true) {
     break;
   } 
   
-  if (Number.isNaN(Number(input)))  {
-    alert('Было введено не число, попробуйте еще раз');
-  }  else if (Number.isNaN(Number(input)) === false) {
+  if (!Number.isNaN(Number(input)))  {
     numbers.push(Number(input));
+  }  else  {
+    alert('Было введено не число, попробуйте еще раз');
   }
-  
-  
 }
 
-for (const number of numbers) {
-  total += number
-}
 
-if (numbers.length !== 0) {
-console.log(`Общая сумма чисел равна ${total}`);
-} else {
+if (numbers.length === 0) {
   alert('Вы не ввели ни одного числа');
-}
+  } else {
+    for (const number of numbers) {
+      total += number
+    }
+    console.log(`Общая сумма чисел равна ${total}`);
+  }
+
 
 
 // Task 2 
 
 const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let attemptsLeft = 3;
-let message;
 
   do {
-  attemptsLeft -= 1;
+  
   const userPassword = prompt('Введите пароль');
 
+  attemptsLeft -= 1;
   if (userPassword === null) break;
   
   if (!attemptsLeft) {
     alert('У вас закончились попытки, аккаунт заблокирован!');
     break;
+  } else if (passwords.includes(userPassword) === true) {
+    alert('Добро пожаловать!');
+    break;
+  } else {
+    alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
   }
-
-  message = (passwords.includes(userPassword)) ? 'Добро пожаловать!' :
-  `Неверный пароль, у вас осталось ${attemptsLeft} попыток`;
-  alert(message);
     
-} while (attemptsLeft || passwords.includes(userPassword) === false)
+} while (attemptsLeft)
+
+
 
 
 
